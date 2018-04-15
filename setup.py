49b002bf6570fc1223743b01dc08f7ae9c9bd7fc
sindrerb <s.r.bilden@fys.uio.no>
2018-04-15 09:55:49 +0200

    Introduced an static operator matrix
    Initially the identity operator calculating bulk plasmons, but can be corrected to favor dipole transitions and so on

--- setup.py
+++ setup.py
@@ -6,16 +6,20 @@ import numpy as np
 
 ext_modules = [ Extension('pyeels.cpyeels', sources = ['./pyeels/_spectrum/spectrum.c'], extra_compile_args=['-std=c99']) ]
 
-#readme = open('README.md','r')
+try:
+      readmefile = open('README.md','r')
+      readme = readmefile.read()
+except:
+      readme = "Python simulation package for EELS volume plasmons"
 
 setup(
       name = 'PyEELS',
-      version = '0.2.0',
+      version = '0.2.1',
       author = "Sindre R. Bilden",
       author_email = "s.r.bilden@fys.uio.no",
       url = 'https://github.com/sindrerb/pyeels',
       description = ("(NB! Still in development) Python package for simulating EELS from band structures"),
-      #long_description = readme.read(),
+      long_description = readme,
       install_requires=['hyperspy', 'pythtb', 'spglib'],
       packages=['pyeels'],
       include_dirs = [np.get_include()],
